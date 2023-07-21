e28c5a993a84321efd76586b775b266401ed5938
Thomas Klijnsma <klijnsma@fnal.gov>
2023-07-21 17:14:38 -0500

    single muon cr

--- plot_trigeff.py
+++ plot_trigeff.py
@@ -1,5 +1,9 @@
 import os, os.path as osp, json, argparse, sys, fnmatch, json
 import numpy as np
+import tqdm
+import scipy.stats
+
+import seutils
 
 import matplotlib.pyplot as plt
 
@@ -8,8 +12,177 @@ import svj_ntuple_processing as svj
 import trigger as trig
 
 
+scripter = trig.Scripter()
 
-def main():
+
+def reverse_cumsum(x):
+    """
+    Cumsum from right to left
+    """
+    return np.cumsum(x[::-1])[::-1]
+
+
+# trig.logger.warning('REMOVING SOME 2016 TITLES FOR NOW')
+# svj.triggers_2016.remove('HLT_PFHT800_v')
+# svj.triggers_2016.remove('HLT_PFHT300_PFMET100_v')
+# svj.triggers_2016.remove('HLT_PFHT300_PFMET110_v')
+
+# svj.triggers_2016.extend([
+#     'HLT_PFMET120_PFMHT120_IDTight_v',
+#     'HLT_PFMETNoMu120_PFMHTNoMu120_IDTight_v',
+#     ])
+# svj.triggers_2018.extend([
+#     'HLT_PFMET120_PFMHT120_IDTight_v',
+#     'HLT_PFMETNoMu120_PFMHTNoMu120_IDTight_v',
+#     ])
+
+svj.triggers_2017.append('HLT_PFMETTypeOne120_PFMHT120_IDTight_v')
+svj.triggers_2018.extend([
+    'HLT_PFMET120_PFMHT120_IDTight_v',
+    'HLT_PFMETNoMu120_PFMHTNoMu120_IDTight_v',
+    ])
+
+
+def poisson_err_up(n):
+    """
+    Python reimplementation of PoissonErrorUp in 
+    https://github.com/kpedro88/Analysis/blob/SVJ2018/KCode/KMath.h
+    """
+    alpha = 1 - 0.682689492
+    # double U = (ROOT::Math::gamma_quantile_c(alpha/2,N+1,1.));
+    # >>> ROOT.Math.gamma_quantile_c(alpha/2., 11, 1.)
+    # 14.266949759891316
+    # >>> scipy.stats.gamma.ppf(1.-alpha/2., 11)
+    # 14.266949759891313
+    u = scipy.stats.gamma.ppf(1-alpha/2., n+1)
+    return u-n
+
+
+def calc_eff_error(passes, total):
+    """
+    Python reimplementation of EffError in 
+    https://github.com/kpedro88/Analysis/blob/SVJ2018/KCode/KMath.h
+    """
+    n = len(passes)
+    nonzero = total != 0.
+
+    passes = passes[nonzero]
+    total = total[nonzero]
+    fail = total - passes
+
+    eff = passes / total
+
+    err_pass = poisson_err_up(passes)
+    err_fail = poisson_err_up(fail)
+
+    # return 
+    #   sqrt(pow(1-eff,2)*pow(err_p,2)
+    #   + pow(eff,2)*pow(err_f,2)) / (double)total;
+    err_eff = np.sqrt(
+        (1.-eff)**2 * err_pass**2
+        + eff**2 * err_fail**2
+        ) / total
+
+    # Limit eff + err < 1., eff - err > 0.
+    err_eff_up = np.where(eff + err_eff <= 1., err_eff, 1.-eff)
+    err_eff_down = np.where(eff - err_eff >= 0., err_eff, eff)
+
+    # Fill in calculated err wherever the total was nonzero
+    err_eff_up_final = np.zeros(n)
+    err_eff_up_final[nonzero] = err_eff_up
+
+    err_eff_down_final = np.zeros(n)
+    err_eff_down_final[nonzero] = err_eff_down
+
+    return np.row_stack((err_eff_down_final, err_eff_up_final))
+
+
+
+@scripter
+def singlemuon():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('npzfiles', type=str, nargs='+')
+    parser.add_argument('--var', type=str, default='pt', choices=trig.variables)
+    parser.add_argument('-y', '--year', type=int, default=2018)
+    args = parser.parse_args()
+
+    # binning = trig.binning[args.var]
+    binning = np.linspace(0., 1200., 60)
+
+    if args.year == 2016:
+        binning = np.concatenate((
+            np.linspace(0., 800., 39, endpoint=False),
+            np.linspace(820., 1200., 10), 
+            ))
+
+    bin_centers = .5*(binning[1:] + binning[:-1])
+
+    ax2_y = 0.
+
+    npzfiles = []
+    for f in args.npzfiles:
+        if seutils.path.has_protocol(f) and '*' in f:
+            npzfiles.extend(seutils.ls_wildcard(f))
+        else:
+            npzfiles.append(f)
+
+    cols = []
+    for f in tqdm.tqdm(npzfiles):
+        cols.append(svj.Columns.load(f, encoding='latin1'))
+    col = svj.concat_columns(cols)
+    del cols
+
+    with trig.quick_ax() as ax:
+
+        passes_singlemuon = trig.filter_triggers(col, triggers=['HLT_Mu50_v'])
+        passes = trig.filter_triggers(col, triggers=args.year) & passes_singlemuon
+
+        vals = col.arrays[args.var]
+
+        h_singlemuon = np.histogram(vals[passes_singlemuon], binning)[0]
+        h_trig = np.histogram(vals[(passes)], binning)[0]
+        ax2_y = max(ax2_y, np.max(h_trig))
+
+        # numerator = reverse_cumsum(h_trig)
+        # denominator = reverse_cumsum(h_singlemuon)
+        numerator = h_trig
+        denominator = h_singlemuon
+        eff = np.where(denominator!=0., numerator/denominator, 0.)
+
+        err_eff = calc_eff_error(numerator, denominator)
+
+
+        color = plt.rcParams['axes.prop_cycle'].by_key()['color'][0]
+        ax.errorbar(bin_centers, eff, yerr=err_eff, fmt='o', color=color)
+
+        # color = ax.plot(bin_centers, eff, 'o')[0].get_color()
+
+        try:
+            interpolation = trig.Interpolation(bin_centers, eff)
+            ax.plot(
+                *interpolation.fine(), c=color,
+                label=f'95\% = {interpolation.solve(.95):.1f} GeV'
+                )
+        except Exception:
+            svj.logger.error(f'Interpolation failed')
+
+        ax2 = ax.twinx()
+        ax2.hist(vals[passes], binning, color=color, alpha=.5)
+        ax2.step(binning[:-1], h_singlemuon, '--', c=color, where='post')
+        
+        ax.set_ylim(0, 1.15)
+        ax2.set_ylim(0., 1.5*ax2_y)
+
+        ax.legend(fontsize=16)
+        trig.put_on_cmslabel(ax, text='Preliminary', year=args.year)
+        ax.set_xlabel(trig.var_titles[args.var])
+        ax.set_ylabel('Efficiency')
+
+
+
+
+@scripter
+def mc():
     parser = argparse.ArgumentParser()
     parser.add_argument('npzfiles', type=str, nargs='+')
     parser.add_argument('--var', type=str, default='pt', choices=trig.variables)
@@ -20,43 +193,58 @@ def main():
     ax = fig.gca()
 
     binning = trig.binning[args.var]
-    # binning = np.linspace(0., 630., 30)
-
     bin_centers = .5*(binning[1:] + binning[:-1])
 
-    for col in [ svj.Columns.load(f) for f in args.npzfiles ]:
-        passes = trig.filter_triggers(col, not(args.nomet))
 
-        vals = col.arrays[args.var]
+    ax2_y = 0.
 
-        revcumsum = lambda x: np.cumsum(x[::-1])[::-1]
-        h_notrig = revcumsum(np.histogram(vals, binning)[0])
-        h_trig = revcumsum(np.histogram(vals[passes], binning)[0])
-        eff = np.where(h_notrig!=0., h_trig/h_notrig, 0.)
+    with trig.quick_ax() as ax:
+        ax2 = ax.twinx()
 
-        meta = col.metadata
-        label = f"mz{meta['mz']}, rinv{meta['rinv']}"
-        line = ax.plot(bin_centers, eff, 'o')[0]
+        for col in [ svj.Columns.load(f) for f in args.npzfiles ]:
+            passes = trig.filter_triggers(col, not(args.nomet))
+            vals = col.arrays[args.var]
 
-        try:
-            interpolation = trig.Interpolation(bin_centers, eff)
-            print(f'Eff for {label} at 500 GeV: {interpolation(500.)}')
-            ax.plot(
-                *interpolation.fine(), c=line.get_color(),
-                label=f'{label} 98\% = {interpolation.solve(.98):.1f} GeV'
-                )
-        except Exception:
-            svj.logger.error(f'Interpolation failed for {label}')
 
-    ax.legend(fontsize=16)
-    trig.put_on_cmslabel(ax)
-    ax.set_xlabel(trig.var_titles[args.var])
-    ax.set_ylabel('Efficiency')
+            h_all = np.histogram(vals, binning)[0]
+            h_trig = np.histogram(vals[passes], binning)[0]
 
-    outfile = 'sigeff.png'
-    plt.savefig(outfile, bbox_inches='tight')
-    os.system(f'imgcat {outfile}')
+            ax2_y = max(ax2_y, np.max(h_trig))
+
+            # numerator = reverse_cumsum(h_trig)
+            # denominator = reverse_cumsum(h_all)
+            numerator = h_trig
+            denominator = h_all
+            eff = np.where(denominator!=0., numerator/denominator, 0.)
+
+            meta = col.metadata
+
+            try:
+                label = f"mz{meta['mz']}, rinv{meta['rinv']}"
+            except KeyError:
+                label = osp.basename(col.metadata['src']).replace('.npz','')
+
+            color = ax.plot(bin_centers, eff, 'o')[0].get_color()
+
+            ax2.hist(vals[passes], binning, color=color, alpha=.5)
+            ax2.step(binning[:-1], h_all, '--', c=color, where='post')
+
+            try:
+                interpolation = trig.Interpolation(bin_centers, eff)
+                print(f'Eff for {label} at 500 GeV: {interpolation(500.)}')
+                ax.plot(
+                    *interpolation.fine(), c=color,
+                    label=f'{label} 98\% = {interpolation.solve(.98):.1f} GeV'
+                    )
+            except Exception:
+                svj.logger.error(f'Interpolation failed for {label}')
+
+        ax2.set_ylim(0., 1.5*ax2_y)
+        ax.legend(fontsize=16)
+        trig.put_on_cmslabel(ax)
+        ax.set_xlabel(trig.var_titles[args.var])
+        ax.set_ylabel('Efficiency')
 
 
 if __name__ == '__main__':
-    main()
+    scripter.run()
--- trigger.py
+++ trigger.py
@@ -1,31 +1,55 @@
 import os, os.path as osp, glob, copy, argparse, sys, uuid, logging, logging.config, re, shutil, pprint
+from contextlib import contextmanager
 
 import numpy as np
 
 import svj_ntuple_processing as svj
 
 
-logging.config.dictConfig({ 
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': { 
-        'standard': { 
-            'format': '\033[94m%(name)s:%(asctime)s:%(levelname)s:%(module)s:%(funcName)s:%(lineno)s:\033[0m %(message)s',
-            'datefmt': '%Y-%m-%d %H:%M:%S'
-            },
-        },
-    'handlers': { 
-        'default': { 
-            'level': 'INFO', 'formatter': 'standard', 'class': 'logging.StreamHandler',
-            'stream': 'ext://sys.stdout',
-            },
-        },
-    'loggers': { 
-        'triggerstudy': {'handlers': ['default'], 'level': 'DEBUG', 'propagate': False},
-        } 
-    })
-logger = logging.getLogger('triggerstudy')
+def setup_logger(name: str = "triggerstudy") -> logging.Logger:
+    """Sets up a Logger instance.
 
+    If a logger with `name` already exists, returns the existing logger.
+
+    Args:
+        name (str, optional): Name of the logger. Defaults to "demognn".
+
+    Returns:
+        logging.Logger: Logger object.
+    """
+    if name in logging.Logger.manager.loggerDict:
+        logger = logging.getLogger(name)
+        logger.info("Logger %s is already defined", name)
+    else:
+        fmt = logging.Formatter(
+            fmt=(
+                "\033[34m[%(name)s:%(levelname)s:%(asctime)s:%(module)s:%(lineno)s]\033[0m"
+                + " %(message)s"
+            ),
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        handler = logging.StreamHandler()
+        handler.setFormatter(fmt)
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.INFO)
+        logger.addHandler(handler)
+    return logger
+
+
+logger = setup_logger()
+
+
+def debug(flag: bool = True) -> None:
+    """Convenience switch to set the logging level to DEBUG.
+
+    Args:
+        flag (bool, optional): If true, set the logging level to DEBUG. Otherwise, set
+            it to INFO. Defaults to True.
+    """
+    if flag:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
 
 
 cms_style = {
@@ -127,17 +151,22 @@ class Interpolation:
         return x, self(x)
 
 
-def filter_triggers(cols, incl_met=True):
-    triggers = svj.triggers_2018
+def filter_triggers(cols, incl_met=True, triggers=svj.triggers_2018):
+    if isinstance(triggers, int):
+        triggers = {
+            2018 : svj.triggers_2018,
+            2017 : svj.triggers_2017,
+            2016 : svj.triggers_2016,
+            }[triggers]
     if not incl_met: triggers = [t for t in triggers if 'MET' not in t]
     indices = np.array([cols.metadata['trigger_titles'].index(t) for t in triggers])
-    return np.any(cols.arrays['triggers'][:,indices], axis=-1)
+    return np.any(cols.arrays['triggers'][:,indices] == 1, axis=-1)
 
 
-def put_on_cmslabel(ax):
+def put_on_cmslabel(ax, text='Simulation Preliminary', year=2018):
     ax.text(
         .0, 1.0,
-        r'\textbf{CMS}\,\fontsize{19pt}{3em}\selectfont{}{\textit{Simulation Preliminary}}',
+        r'\textbf{CMS}\,\fontsize{19pt}{3em}\selectfont{}{\textit{'+text+'}}',
         horizontalalignment='left',
         verticalalignment='bottom',
         transform=ax.transAxes,
@@ -146,7 +175,7 @@ def put_on_cmslabel(ax):
         )
     ax.text(
         1.0, 1.0,
-        r'2018 (13 TeV)',
+        '{} (13 TeV)'.format(year),
         horizontalalignment='right',
         verticalalignment='bottom',
         transform=ax.transAxes,
@@ -176,3 +205,120 @@ binning = dict(
     )
 
 variables = list(sorted(binning.keys()))
+
+
+
+
+def pull_arg(*args, **kwargs) -> argparse.Namespace:
+    """Reads a specific argument out of sys.argv, and then deletes that argument from
+    sys.argv.
+
+    This useful to build very adaptive command line options to scripts. It does
+    sacrifice auto documentation of the command line options though.
+
+    Returns:
+        argparse.Namespace: Namespace object for only the specific argument.
+    """
+
+    """
+    Reads a specific argument out of sys.argv, and then
+    deletes that argument from sys.argv.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(*args, **kwargs)
+    args, other_args = parser.parse_known_args()
+    sys.argv = [sys.argv[0]] + other_args
+    return args
+
+
+@contextmanager
+def timeit(msg):
+    """
+    Prints duration of a block of code in the terminal.
+    """
+    try:
+        logger.info(msg)
+        sys.stdout.flush()
+        t0 = time.time()
+        yield None
+    finally:
+        t1 = time.time()
+        logger.info(f"Done {msg[0].lower() + msg[1:]}, took {t1-t0:.2f} secs")
+
+
+class Scripter:
+    """
+    Command line utility.
+
+    When an instance of this class is used as a contextwrapper on a function, that
+    function will be considered a 'command'.
+
+    When Scripter.run() is called, the script name is pulled from the command line, and
+    the corresponding function is executed.
+
+    Example:
+
+        In file test.py:
+        >>> scripter = Scripter()
+        >>> @scripter
+        >>> def my_func():
+        >>>     print('Hello world!')
+        >>> scripter.run()
+
+        On the command line, the following would print 'Hello world!':
+        $ python test.py my_func
+    """
+
+    def __init__(self):
+        self.scripts = {}
+
+    def __call__(self, fn):
+        """
+        Stores a command line script with its name as the key.
+        """
+        self.scripts[fn.__name__] = fn
+        return fn
+
+    def run(self):
+        script = pull_arg("script", choices=list(self.scripts.keys())).script
+        logger.info(f"Running {script}")
+        self.scripts[script]()
+
+
+@contextmanager
+def quick_ax(figsize=(10, 10), outfile="tmp.png"):
+    """
+    Context manager to open a matplotlib Axes.
+    Upon closing, saves it to a file and calls
+    imgcat (an iTerm2 command line util) on it
+    to display the plot in the terminal.
+    """
+    try:
+        fig = plt.figure(figsize=figsize)
+        ax = fig.gca()
+        yield ax
+    finally:
+        plt.savefig(outfile, bbox_inches="tight")
+        try:
+            os.system(f"imgcat {outfile}")
+        except Exception:
+            pass
+
+
+@contextmanager
+def quick_fig(figsize=(10, 10), outfile="tmp.png"):
+    """
+    Context manager to open a matplotlib Figure.
+    Upon closing, saves it to a file and calls
+    imgcat (an iTerm2 command line util) on it
+    to display the plot in the terminal.
+    """
+    try:
+        fig = plt.figure(figsize=figsize)
+        yield fig
+    finally:
+        plt.savefig(outfile, bbox_inches="tight")
+        try:
+            os.system(f"imgcat {outfile}")
+        except Exception:
+            pass
